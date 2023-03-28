7a1f3f577c5994d9c7d70e52f89b89374eec67be
Manu S <ms.manu.856@gmail.com>
2023-03-28 13:23:40 +0530

    final commit

---
+++ emp_details_add/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "emp_details_add",
+
+    'summary': """
+        Adds new fields to the Employee model""",
+
+    'description': """
+    This module adds new fields to the Employee model, including:
+        - Skills/Area
+        - Project Name
+        - Project Start Date
+        - Project End Date
+        - HR POC
+        - PM
+        - PPM
+        - Experience    """,
+
+    'author': "Leuwint Technology",
+    'website': "https://www.leuwint.com/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Human Resources',
+    'version': '1.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'hr'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/work_location_views.xml',
+        
+    ],
+    'installable': True,
+    'auto_install'  : False,
+   # only loaded in demonstration mode
+    # 'demo': [
+    #     'demo/demo.xml', 
+    # ],
+    
+}
---
+++ emp_details_add/models/models.py
@@ -0,0 +1,13 @@
+from odoo import models, fields
+
+class HrEmployee(models.Model):
+    _inherit = 'hr.employee'
+
+    skills_name = fields.Char(string="Skills/Area")
+    project_name = fields.Char(string='Project Name')
+    project_start_date = fields.Date(string='Project Start Date')
+    project_end_date = fields.Date(string='Project End Date')
+    hr_poc = fields.Char(string='HR POC')
+    pm = fields.Char(string='PM')
+    ppm = fields.Char(string='PPM')
+    experience = fields.Float(string='Experience')
